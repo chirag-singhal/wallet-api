18ff19693757db4d78fa87fd5930bc89100a3ddb
mukul0596 <icecoolmkl@gmail.com>
2019-06-15 22:19:38 +0530

    reset local branch

--- jwtverify.js
+++ jwtverify.js
@@ -4,31 +4,30 @@ const config = require('./config.js');
 const checkToken = (req, res, next) => {
     if(req.path != '/auth/signup' && req.path != '/auth/login'){
         const token = req.headers['x-access-token'] || req.headers['authorization']; // Express headers are auto converted to lowercase
-        if (token.startsWith('Bearer ')) {
-            // Remove Bearer from string
-            token = token.slice(7, token.length);
-        }
+    if (token.startsWith('Bearer ')) {
+        // Remove Bearer from string
+        token = token.slice(7, token.length);
+    }
 
-        if (token) {
-            jwt.verify(token, config.secret, (err, decoded) => {
-            if (err) {
-                return res.json({
-                success: false,
-                message: 'Token is not valid'
-                });
-            } else {
-                req.decoded = decoded;
-                next();
-            }
-            });
-        } else {
+    if (token) {
+        jwt.verify(token, config.secret, (err, decoded) => {
+        if (err) {
             return res.json({
             success: false,
-            message: 'Auth token is not supplied'
+            message: 'Token is not valid'
             });
+        } else {
+            req.decoded = decoded;
+            next();
         }
+        });
+    } else {
+        return res.json({
+        success: false,
+        message: 'Auth token is not supplied'
+        });
     }
-    else next();
+    }
 };
 
 module.exports = checkToken
