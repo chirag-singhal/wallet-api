a8332cdc44201974706be8f64767a1ffe615b94a
chirag-singhal <csinghal208@gmail.com>
2019-07-14 11:39:06 +0530

    added offerer

--- models/offerer.js
+++ models/offerer.js
@@ -13,12 +13,12 @@ const OffererSchema = new mongoose.Schema({
 var Offerer = mongoose.model('offerers', OffererSchema)
 
 
-// Offerers.create({
-//     offererName: "Test Actor",
-//     offererImage: "../images/RBF-SNC-25D-600x600.gif"
-// }).then((offer) => {
-//     console.log(offer)
-// })
-// .catch((err) => console.log(err))
+Offerers.create({
+    offererName: "Test Actor",
+    offererImage: "../images/RBF-SNC-25D-600x600.gif"
+}).then((offer) => {
+    console.log(offer)
+})
+.catch((err) => console.log(err))
 
 module.exports = Offerer
