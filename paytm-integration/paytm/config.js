612707ccd2e629eaaa392df75b08d587fec556fd
chirag-singhal <csinghal208@gmail.com>
2019-07-15 18:38:00 +0530

    paytm callback url changed

--- paytm-integration/paytm/config.js
+++ paytm-integration/paytm/config.js
@@ -6,6 +6,6 @@ module.exports = {
   CHANNEL_ID: "WAP",
   INDUSTRY_TYPE_ID: "Retail",
   CALLBACK_URL: "http://ec2-34-207-68-63.compute-1.amazonaws.com:3000/payWithPaytmResponse",
-  CALLBACK_URL_ADD: "http://ec2-34-207-68-63.compute-1.amazonaws.com:3000/addToWalletResponse",
+  CALLBACK_URL_ADD: "https://pguat.paytm.com/paytmchecksum/paytmCallback.jsp",
   PORT: 3000
 };
