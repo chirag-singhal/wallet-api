52c5c853553608e7dca7aa9142138c76fd02e893
chirag-singhal <csinghal208@gmail.com>
2019-07-18 21:54:26 +0530

    test data commented

--- models/shopingCategory.js
+++ models/shopingCategory.js
@@ -66,251 +66,128 @@ Product.virtual('offerers', {
 });
 
 
-const category = new Category({
-    title: "merchant",
-    products: [
-        {
-            title: "Dupatta-1",
-            ikcPrice: 699,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Dupatta-1.",
-            imageUrl: "../images/RBF-SNC-25D-600x600.gif",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Dupatta-2",
-            ikcPrice: 549,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Dupatta-2.",
-            imageUrl: "../images/RBF-BAL-03A-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Suit-1",
-            ikcPrice: 520,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Suit-1.",
-            imageUrl: "../images/RBF-SUT-05-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Suit-2",
-            ikcPrice: 449,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Suit-2.",
-            imageUrl: "../images/RBF-SUT-04-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Kurti-1",
-            ikcPrice: 360,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Kurti-1.",
-            imageUrl: "../images/RBF-SUT-03A-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Kurti-2",
-            ikcPrice: 749,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Kurti-2.",
-            imageUrl: "../images/RBF-KUR-04a-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Saree-1",
-            ikcPrice: 0,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Saree-1.",
-            imageUrl: "../images/RBF-SAR-PINK-01-600x600.gif",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Saree-2",
-            ikcPrice: 1450,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Saree-2.",
-            imageUrl: "../images/Saree-Red-600x600.gif",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        }
-    ]
-});
-category.save().then(() => {
-    console.log(category);
-}).catch((e) => {
-    console.log(e);
-});
-
-const category = new Category({
-    title: "sports talent",
-    products: [
-        {
-            title: "Dupatta-1",
-            ikcPrice: 699,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Dupatta-1.",
-            imageUrl: "../images/RBF-SNC-25D-600x600.gif",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Dupatta-2",
-            ikcPrice: 549,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Dupatta-2.",
-            imageUrl: "../images/RBF-BAL-03A-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Suit-1",
-            ikcPrice: 520,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Suit-1.",
-            imageUrl: "../images/RBF-SUT-05-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Suit-2",
-            ikcPrice: 449,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Suit-2.",
-            imageUrl: "../images/RBF-SUT-04-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Kurti-1",
-            ikcPrice: 360,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Kurti-1.",
-            imageUrl: "../images/RBF-SUT-03A-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Kurti-2",
-            ikcPrice: 749,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Kurti-2.",
-            imageUrl: "../images/RBF-KUR-04a-600x600.jpg",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Saree-1",
-            ikcPrice: 0,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Saree-1.",
-            imageUrl: "../images/RBF-SAR-PINK-01-600x600.gif",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        },
-        {
-            title: "Saree-2",
-            ikcPrice: 1450,
-            inrPrice: 999,
-            discount: 0,
-            description: "This  is some random shit for Saree-2.",
-            imageUrl: "../images/Saree-Red-600x600.gif",
-            stock: 5,
-            isReplaceable: true,
-            isRefundable: true,
-            offererId: '5d2ac52be0d3ad14b114efe2',
-            offererName: 'Test Actor',
-            offererImage: '../images/RBF-SNC-25D-600x600.gif'
-        }
-    ]
-});
-category.save().then(() => {
-    console.log(category);
-}).catch((e) => {
-    console.log(e);
-});
+// const category = new Category({
+//     title: "E-talent",
+//     products: [
+//         {
+//             title: "Dupatta-1",
+//             ikcPrice: 699,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Dupatta-1.",
+//             imageUrl: "../images/RBF-SNC-25D-600x600.gif",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Dupatta-2",
+//             ikcPrice: 549,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Dupatta-2.",
+//             imageUrl: "../images/RBF-BAL-03A-600x600.jpg",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Suit-1",
+//             ikcPrice: 520,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Suit-1.",
+//             imageUrl: "../images/RBF-SUT-05-600x600.jpg",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Suit-2",
+//             ikcPrice: 449,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Suit-2.",
+//             imageUrl: "../images/RBF-SUT-04-600x600.jpg",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Kurti-1",
+//             ikcPrice: 360,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Kurti-1.",
+//             imageUrl: "../images/RBF-SUT-03A-600x600.jpg",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Kurti-2",
+//             ikcPrice: 749,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Kurti-2.",
+//             imageUrl: "../images/RBF-KUR-04a-600x600.jpg",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Saree-1",
+//             ikcPrice: 0,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Saree-1.",
+//             imageUrl: "../images/RBF-SAR-PINK-01-600x600.gif",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         },
+//         {
+//             title: "Saree-2",
+//             ikcPrice: 1450,
+//             inrPrice: 999,
+//             discount: 0,
+//             description: "This  is some random shit for Saree-2.",
+//             imageUrl: "../images/Saree-Red-600x600.gif",
+//             stock: 5,
+//             isReplaceable: true,
+//             isRefundable: true,
+//             offererId: '5d2ac52be0d3ad14b114efe2',
+//             offererName: 'Test Actor',
+//             offererImage: '../images/RBF-SNC-25D-600x600.gif'
+//         }
+//     ]
+// });
+// category.save().then(() => {
+//     console.log(category);
+// }).catch((e) => {
+//     console.log(e);
+// });
 
 
 module.exports = Category;
