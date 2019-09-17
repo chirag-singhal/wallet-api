cad9d22ab90fbfced7d5e0e9362f3376bf78f593
chirag-singhal <csinghal208@gmail.com>
2019-09-17 11:40:27 +0530

    password forgot

--- api/changePassword.js
+++ api/changePassword.js
@@ -10,31 +10,37 @@ changePassword.use(bodyParser.json())
 
 changePassword.route('/')
     .post((req, res, next) => {
+        console.log(req.body)
         if (req.body.token && req.body.password && req.body.otp) {
             resetPassword.findOne({ "token": req.body.token })
                 .then((reset) => {
+                    console.log(reset)
                     Users.findOne({ "contact": reset.contact })
                         .then((user) => {
+                            console.log(user)
                             if (user != null) {
                                 if (reset != null) {
                                     const time = new Date()
                                     time.setSeconds(time.getSeconds() - 300)
                                     if (reset.updatedAt > time) {
-                                        verifyOtp(user.contact, req.body.otp, function(result) {
+                                        verifyOtp(user.contact, req.body.otp, async function (result) {
                                             console.log(result, result)
                                             if (result) {
+                                                console.log(req.body.password)
+                                                let hashPassword;
                                                 bcrypt.hash(req.body.password, 10)
                                                     .then((hashedPassword) => {
-                                                        user.password = hashedPassword;
+                                                        console.log(hashedPassword)
+                                                        hashPassword = hashedPassword;
+                                                        user.password = hashPassword;
+                                                        user.save().then((saved) => {
+                                                            console.log(saved)
+                                                            res.json({ "message": "Password Updated" })
+                                                        })
+                                                            .catch((err) => next(err))
                                                     })
                                                     .catch((err) => next(err))
-                                                user.save()
-                                                    .then(() => {
-                                                        res.statusCode = 200;
-                                                        res.setHeader('Content-Type', 'application/json');
-                                                        res.json({ "message": "Password changed" });
-                                                    })
-                                                    .catch((err) => next(err))
+
                                             }
                                             else {
                                                 res.statusCode = 403;
@@ -48,19 +54,19 @@ changePassword.route('/')
                                     else {
                                         res.statusCode = 403;
                                         res.setHeader('Content-Type', 'application/json');
-                                        res.json({"message": "Token Expired" });
+                                        res.json({ "message": "Token Expired" });
                                     }
                                 }
                                 else {
                                     res.statusCode = 403;
                                     res.setHeader('Content-Type', 'application/json');
-                                    res.json({"message": "Token not found"});
+                                    res.json({ "message": "Token not found" });
                                 }
                             }
                             else {
                                 res.statusCode = 403;
                                 res.setHeader('Content-Type', 'application/json');
-                                res.json({"message": "User not found"});
+                                res.json({ "message": "User not found" });
                             }
                         })
                 })
@@ -74,7 +80,7 @@ changePassword.route('/')
         else {
             res.statusCode = 403;
             res.setHeader('Content-Type', 'application/json');
-            res.json({"message": "Missing Fields"});
+            res.json({ "message": "Missing Fields" });
         }
     })
 
