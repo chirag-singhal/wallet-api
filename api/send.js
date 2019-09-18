69215c8c7449bd80379a5868f66e5737f0af5385
chirag-singhal <csinghal208@gmail.com>
2019-09-18 15:28:47 +0530

    bugs

--- api/send.js
+++ api/send.js
@@ -14,61 +14,60 @@ send.route('/')
                     $inc: { amount: -req.body.amount }
                 }).then((saved) => {
 
+                    User.findOne({ contact: req.body.contact }).then((user) => {
+                        if (user) {
+                            User.findOneAndUpdate({ contact: req.body.contact }, {
+                                $inc: { amount: +req.body.amount }
+                            }).then(() => {
+                                User.findByIdAndUpdate(req.user._id, {
+                                    $push: {
+                                        transactions: {
+                                            transactionId: shortid.generate(),
+                                            amount: -req.body.amount,
+                                            transactionStatus: 'TXN_SUCCESS',
+                                            name: user.username,
+                                            contact: user.contact,
+                                            paymentType: 'ikc',
+                                            detail: "Sent to " + req.body.contact,
+                                            time: Date.now()
+                                        }
+                                    }
+                                }).then(() => {
+                                    User.findOneAndUpdate({ contact: req.body.contact }, {
+                                        $push: {
+                                            transactions: {
+                                                transactionId: shortid.generate(),
+                                                amount: +req.body.amount,
+                                                transactionStatus: 'TXN_SUCCESS',
+                                                name: req.user.username,
+                                                contact: req.user.contact,
+                                                paymentType: 'ikc',
+                                                detail: "Received from " + req.user.contact,
+                                                time: Date.now()
+                                            }
+                                        }
+                                    }).then(() => {
+                                        console.log('saved')
+                                        res.statusCode = 200;
+                                        res.json({ "message": "ikc successfully transferred!" })
+                                    }).catch((err) => next(err))
+                                }).catch((err) => next(err))
+                            }).catch((err) => next(err))
+
+                        }
+                    }).catch((err) => {
+                        res.statusCode = 403;
+                        res.json(err);
+                    })
                 })
                     .catch((err) => next(err))
             }
             else {
                 res.statusCode = 403;
-                res.send({"message": 'Insufficient Balance'})
+                res.send({ "message": 'Insufficient Balance' })
                 res.end();
             }
         }).catch((err) => next(err))
-
-        User.findOne({ contact: req.body.contact }).then((user) => {
-            if (user) {
-                User.findOneAndUpdate({ contact: req.body.contact }, {
-                    $inc: { amount: +req.body.amount }
-                }).then(() => {
-                    User.findByIdAndUpdate(req.user._id, {
-                        $push: {
-                            transactions: {
-                                transactionId: shortid.generate(),
-                                amount: -req.body.amount,
-                                transactionStatus: 'TXN_SUCCESS',
-                                name: user.username,
-                                contact: user.contact,
-                                paymentType: 'ikc',
-                                detail: "Sent to " + req.body.contact,
-                                time: Date.now()
-                            }
-                        }
-                    }).then(() => {
-                        User.findOneAndUpdate({ contact: req.body.contact }, {
-                            $push: {
-                                transactions: {
-                                    transactionId: shortid.generate(),
-                                    amount: +req.body.amount,
-                                    transactionStatus: 'TXN_SUCCESS',
-                                    name: req.user.username,
-                                    contact: req.user.contact,
-                                    paymentType: 'ikc',
-                                    detail: "Received from " + req.user.contact,
-                                    time: Date.now()
-                                }
-                            }
-                        }).then(() => {
-                            console.log('saved')
-                            res.statusCode = 200;
-                            res.json({ "message": "ikc successfully transferred!" })
-                        }).catch((err) => next(err))
-                    }).catch((err) => next(err))
-                }).catch((err) => next(err))
-
-            }
-        }).catch((err) => {
-            res.statusCode = 403;
-            res.json(err);
-        })
     })
 
 module.exports = send
