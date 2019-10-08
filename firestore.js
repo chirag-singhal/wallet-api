d1bc480d4d08968e1c6c4fb923b8297dc5735da3
chirag-singhal <csinghal208@gmail.com>
2019-10-08 10:57:48 +0530

    firestore change

--- firestore.js
+++ firestore.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('./ikc-deal-64088-8d60df02a979.json');
+const serviceAccount = require('./ikc-deal-255305-e8200193b893.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
