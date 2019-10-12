92ae699d7f918dcdcbb55804c8b5533081639f66
chirag-singhal <csinghal208@gmail.com>
2019-10-12 10:17:26 +0530

    update fire

--- firestore.js
+++ firestore.js
@@ -2,19 +2,20 @@ const admin = require('firebase-admin');
 
 const serviceAccount = require('./ikc-deal-e803e-6ffce9b07334.json');
 
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const db = admin.firestore();
+
 const firestore = async (contact, transaction, amount) => {
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount)
-// });
-
-// const db = admin.firestore();
-  // await db.collection('users').doc(''+contact).update({
-  //   transactions: admin.firestore.FieldValue.arrayUnion({
-  //      transaction
-  //   }),
-  //   amount: amount
-  // })
+  await db.collection('users').doc(''+contact).update({
+    transactions: admin.firestore.FieldValue.arrayUnion({
+       transaction
+    }),
+    amount: amount
+  })
 }
 
 module.exports = firestore
