69258ed792c191e614ff6b2755c810f9ac55bc57
chirag-singhal <csinghal208@gmail.com>
2019-10-12 09:14:05 +0530

    firestore'

--- firestore.js
+++ firestore.js
@@ -4,9 +4,9 @@ const serviceAccount = require('./ikc-deal-e803e-6ffce9b07334.json');
 
 const firestore = async (contact, transaction, amount) => {
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount)
+// });
 
 const db = admin.firestore();
   // await db.collection('users').doc(''+contact).update({
