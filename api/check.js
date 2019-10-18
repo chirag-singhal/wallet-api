e6360b8ae0daa316746b2ac6fa6e6228d19d788a
chirag-singhal <csinghal208@gmail.com>
2019-10-18 22:56:01 +0530

    console

--- api/check.js
+++ api/check.js
@@ -9,7 +9,7 @@ check.use(bodyParser.json());
 
 check.route('/')
 .post((req, res, next) => {
-    Users.findOne({"conatct": req.body.conatct}).then((user) => {
+    Users.findOne({"conatct": req.body.contact}).then((user) => {
         console.log(user)
         console.log(req.body)
         if(user) {
