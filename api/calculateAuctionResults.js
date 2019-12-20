bffbd97d07ff047ef8177ed12b902b80d90dcafc
chirag-singhal <csinghal208@gmail.com>
2019-12-20 23:14:36 +0530

    assign

--- api/calculateAuctionResults.js
+++ api/calculateAuctionResults.js
@@ -3,40 +3,50 @@ const bodyParser = require('body-parser');
 
 const AuctionVendor = require('../models/auctionVendor');
 const User = require('../models/users');
+const AuctionProduct = require('../models/auctionProducts');
 
 const calculateResults = express.Router();
 
 calculateResults.route('/')
-.post((req, res, next) => {
-    AuctionVendor.findById(req.user._id).then(async (auctionVendor) => {
-        for(let j = 0; j < auctionVendor.auctions.length; j++) {
-            if(auctionVendor.auctions[j].auctionId == req.body.auctionId) {
-                auctionVendor.auctions[j].results = true;
-                for(let i = 0; i < auctionVendor.auctions[j].bid.length; i++) {
-                    User.findById(auctionVendor.auctions[j].bid[i].userId).then(async (user) => {
-                        for(let i = 0; i < user.bids.length; i++) {
-                            if(user.bids[i].auctionId == req.body.auctionId){
-                                user.bids[i].winner = "lost";
-                                break;
+    .post((req, res, next) => {
+        AuctionVendor.findById(req.user._id).then(async (auctionVendor) => {
+            for (let j = 0; j < auctionVendor.auctions.length; j++) {
+                if (auctionVendor.auctions[j].auctionId == req.body.auctionId) {
+                    if (auctionVendor.auctions[j].results) {
+                        res.statusCode = 403;
+                        res.json({
+                            "message": "Result already calculated"
+                        });
+                        res.end();
+                    } else {
+                        auctionVendor.auctions[j].results = true;
+                        for (let i = 0; i < auctionVendor.auctions[j].bid.length; i++) {
+                            User.findById(auctionVendor.auctions[j].bid[i].userId).then(async (user) => {
+                                for (let i = 0; i < user.bids.length; i++) {
+                                    if (user.bids[i].auctionId == req.body.auctionId) {
+                                        user.bids[i].winner = "lost";
+                                        break;
+                                    }
+                                }
+                                await user.save();
+                            })
+                        }
+                        User.findById(auctionVendor.auctions[j].winner.winner).then(async (user) => {
+                            for (let i = 0; i < user.bids.length; i++) {
+                                if (user.bids[i].auctionId == req.body.auctionId) {
+                                    user.bids[i].winner = "won";
+                                    break;
+                                }
                             }
-                        }
-                        await user.save();
-                    })
-                }
-                User.findById(auctionVendor.auctions[j].winner.winner).then(async (user) => {
-                    for(let i = 0; i < user.bids.length; i++) {
-                        if(user.bids[i].auctionId == req.body.auctionId){
-                            user.bids[i].winner = "won";
-                            break;
-                        }
+                            await user.save();
+                        })
                     }
-                    await user.save();
-                })
+                }
             }
-        }
-        await auctionVendor.save();
-        res.end();
+            await AuctionProduct.deleteById(req.body.auctionId);
+            await auctionVendor.save();
+            res.end();
+        })
     })
-})
 
 module.exports = calculateResults
--- api/placeBid.js
+++ api/placeBid.js
@@ -8,10 +8,13 @@ const placeBid = (req, res) => {
     // const bidRemark = req.body.bidRemark;
     const userId = req.user._id;
     console.log(userId)
+    let placed = false;
     const winner = false;
     AuctionProduct.findById(productId).then((product) => {
         if (!product) {
-            return res.status(500).json({ "message": "Invalid Product Id!" });
+            return res.status(500).json({
+                "message": "Invalid Product Id!"
+            });
         }
 
         return AuctionProduct.findById(req.body.productId)
@@ -20,103 +23,110 @@ const placeBid = (req, res) => {
         for (bid of product.bid) {
             if (bid.userId == req.user._id) {
                 console.log(user)
-                return res.json({ "message": "Bid already placed!" });
+                placed = true;
+                res.json({
+                    "message": "Bid already placed!"
+                });
+                res.end();
             }
         }
-        AuctionVendor.findById(product.auctionCreator).then((auctionVendor) => {
-            if (product.bid.length == 0) {
-                AuctionProduct.findByIdAndUpdate(productId, {
-                    $set: {
-                        "winner": {
-                            bidAmount,
-                            userId
-                        }
-                    }
-                }).then(() => {
-                    for (let i = 0; i < auctionVendor.auctions.length; i++) {
-                        if (auctionVendor.auctions[i].auctionId == productId) {
-                            const winner = {
-                                "winner": userId,
-                                "bidAmount": bidAmount
-                            }
-                            auctionVendor.auctions[i].winner = winner;
-                            break;
-                        }
-                    }
-                    auctionVendor.save().then((vendor) => {
-                        console.log(vendor);
-                    })
-                })
-            }
-            else if (product.winner.bidAmount < bidAmount) {
-                AuctionProduct.findByIdAndUpdate(productId, {
-                    $set: {
-                        "winner": {
-                            bidAmount,
-                            "winner": userId
-                        }
-                    }
-                }).then(() => {
-                    for (let i = 0; i < auctionVendor.auctions.length; i++) {
-                        if (auctionVendor.auctions[i].auctionId == productId) {
-                            const winner = {
-                                "winner": userId,
-                                "bidAmount": bidAmount
-                            }
-                            auctionVendor.auctions[i].winner = winner;
-                            break;
-                        }
-                    }
-                    auctionVendor.save().then((vendor) => {
-                        console.log(vendor);
-                    })
-                })
-            }
+        if (!placed) {
             AuctionVendor.findById(product.auctionCreator).then((auctionVendor) => {
-                for (let i = 0; i < auctionVendor.auctions.length; i++) {
-                    if (auctionVendor.auctions[i].auctionId == productId) {
-                        auctionVendor.auctions[i].bid.push({
-                            "userId": userId,
-                            "bidAmount": bidAmount
-                        })
-                    }
-                    break;
-                }
-                auctionVendor.save().then((vendor) => {
-                    console.log(vendor);
-                })
-            })
-            User.findByIdAndUpdate(userId, {
-                $push: {
-                    "bids": {
-                        "auctionId": productId,
-                        "name": product.title,
-                        bidAmount,
-                        "winner": "On Going"
-                    }
-                }
-            }).then(() => {
-                console.log("user model bid ")
-                AuctionProduct.findByIdAndUpdate(productId, {
-                    $push: {
-                        "bid": {
-                            bidAmount,
-                            userId
+                if (product.bid.length == 0) {
+                    AuctionProduct.findByIdAndUpdate(productId, {
+                        $set: {
+                            "winner": {
+                                bidAmount,
+                                userId
+                            }
                         }
-                    },
-                    $inc: {
-                        "numberOfBids": 1
+                    }).then(() => {
+                        for (let i = 0; i < auctionVendor.auctions.length; i++) {
+                            if (auctionVendor.auctions[i].auctionId == productId) {
+                                const winner = {
+                                    "winner": userId,
+                                    "bidAmount": bidAmount
+                                }
+                                auctionVendor.auctions[i].winner = winner;
+                                break;
+                            }
+                        }
+                        auctionVendor.save().then((vendor) => {
+                            console.log(vendor);
+                        })
+                    })
+                } else if (product.winner.bidAmount < bidAmount) {
+                    AuctionProduct.findByIdAndUpdate(productId, {
+                        $set: {
+                            "winner": {
+                                bidAmount,
+                                "winner": userId
+                            }
+                        }
+                    }).then(() => {
+                        for (let i = 0; i < auctionVendor.auctions.length; i++) {
+                            if (auctionVendor.auctions[i].auctionId == productId) {
+                                const winner = {
+                                    "winner": userId,
+                                    "bidAmount": bidAmount
+                                }
+                                auctionVendor.auctions[i].winner = winner;
+                                break;
+                            }
+                        }
+                        auctionVendor.save().then((vendor) => {
+                            console.log(vendor);
+                        })
+                    })
+                }
+                AuctionVendor.findById(product.auctionCreator).then((auctionVendor) => {
+                    for (let i = 0; i < auctionVendor.auctions.length; i++) {
+                        if (auctionVendor.auctions[i].auctionId == productId) {
+                            auctionVendor.auctions[i].bid.push({
+                                "userId": userId,
+                                "bidAmount": bidAmount
+                            })
+                        }
+                        break;
                     }
+                    auctionVendor.save().then((vendor) => {
+                        console.log(vendor);
+                    })
                 })
-                    .then((bid) => {
-                        console.log(bid)
-                        res.json({ "messgage": "Bid successfully placed!" });
-                    }).catch((e) => {
-                        console.log(e);
-                        res.send(e);
-                    });
+                User.findByIdAndUpdate(userId, {
+                    $push: {
+                        "bids": {
+                            "auctionId": productId,
+                            "name": product.title,
+                            bidAmount,
+                            "winner": "On Going"
+                        }
+                    }
+                }).then(() => {
+                    console.log("user model bid ")
+                    AuctionProduct.findByIdAndUpdate(productId, {
+                            $push: {
+                                "bid": {
+                                    bidAmount,
+                                    userId
+                                }
+                            },
+                            $inc: {
+                                "numberOfBids": 1
+                            }
+                        })
+                        .then((bid) => {
+                            console.log(bid)
+                            res.json({
+                                "messgage": "Bid successfully placed!"
+                            });
+                        }).catch((e) => {
+                            console.log(e);
+                            res.send(e);
+                        });
+                })
             })
-        })
+        }
     }).catch((e) => {
         console.log(e);
         res.json(e);
