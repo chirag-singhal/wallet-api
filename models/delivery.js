80d038faecc67bff354e2cefcd862c8fb1efade7
chirag-singhal <csinghal208@gmail.com>
2019-12-21 22:29:27 +0530

    assign

--- models/delivery.js
+++ models/delivery.js
@@ -201,7 +201,7 @@ const ShoppingOrderSchema = new mongoose.Schema({
     product: {
         type: ProductSchema
     },
-    DeliveryAddress: {
+    deliveryAddress: {
         type: DeliveryAddressSchema
     },
     amount: {
